6d549223b5ee81c7b73aadc6057b915369021362
Mjeanbapti <marcjeanb0@gmail.com>
2021-08-29 21:08:31 -0400

    API - Orders 1

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit{
   }
   loadCurrentUser() {
     const token = localStorage.getItem('token');
-      this.accountService.loadCurrentUser(token!)!.subscribe(() => {
+      this.accountService.loadCurrentUser(token!).subscribe(() => {
         console.log('loaded user');
       }, error => {
         console.log(error);
